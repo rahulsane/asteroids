1ee9a63c6fb16b48b5fef4012a5f1b270f978569
rahulsane <rahulshirishsane@gmail.com>
2025-01-02 16:56:56 -0500

    created player class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import Player
 
 def main() -> None:
     print("Starting asteroids!")
@@ -11,12 +12,14 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
     running = True
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("purple")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
