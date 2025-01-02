7af30a1c0ff01cf381a0c92ba30b78aa70aa31af
rahulsane <rahulshirishsane@gmail.com>
2025-01-02 17:23:35 -0500

    added player movement and added groups

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
     running = True
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
