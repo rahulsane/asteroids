b85643412b53801ca20f58d744af77e3f689309d
rahulsane <rahulshirishsane@gmail.com>
2025-01-02 15:58:51 -0500

    added pygame skeleton and added pycache to gitignore

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from constants import *
 
 def main() -> None:
     print("Starting asteroids!")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("purple")
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
